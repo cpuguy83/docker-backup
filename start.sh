57db0e37bde158a7c8dbe007546d549924518017
bgoff <bgoff@tpa0.envu.io>
2014-05-05 16:50:04 -0400

    Add line that sends env vars to .env for cron

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Do this so we can read these vars in cron
+env > /Backup/.env
+
 whenever -f /Backup/schedule.rb --clear-crontab
 whenever -f /Backup/schedule.rb --write-crontab
 exec cron -f
