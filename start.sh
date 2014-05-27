85f19735a2f23e064ebd6a0f423f83b52fd0a47e
Brian Goff <cpuguy83@gmail.com>
2014-05-27 12:43:45 -0400

    Make sure docker bin is placed before schedule is created

--- start.sh
+++ start.sh
@@ -3,6 +3,12 @@
 # Do this so we can read these vars in cron
 env > /Backup/.env
 
+[[ -f /usr/bin/docker ]] ||
+  cp /.dockerinit /usr/bin/docker
+
 whenever -f /Backup/schedule.rb --clear-crontab
 whenever -f /Backup/schedule.rb --write-crontab
+
+
+
 exec cron -f
